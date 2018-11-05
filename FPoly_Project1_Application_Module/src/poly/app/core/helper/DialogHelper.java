fe540deefecb10d305626e4ecda1e9a8fb9e0f76
devlight97 <dev.light97@gmail.com>
2018-11-05 08:11:36 +0700

    Cap nhat cac chuc nang co ban cua PhimJframe

--- FPoly_Project1_Application_Module/src/poly/app/core/helper/DialogHelper.java
+++ FPoly_Project1_Application_Module/src/poly/app/core/helper/DialogHelper.java
@@ -8,27 +8,27 @@ import javax.swing.JOptionPane;
 
 public class DialogHelper {
 
-    private static URL urlIconInfo = DialogHelper.class.getResource("../../view/icon/info-success.png");
-    private static URL urlIconError = DialogHelper.class.getResource("../../view/icon/info-error.png");
+//    private static URL urlIconInfo = DialogHelper.class.getResource("../../view/icon/info-success.png");
+//    private static URL urlIconError = DialogHelper.class.getResource("../../view/icon/info-error.png");
 
     public static final int INFORMATION_MESSAGE = 1;
     public static final int ERROR_MESSAGE = 0;
 
     public static void message(Component parent, String message, int messageType) {
-        URL urlIcon = messageType == INFORMATION_MESSAGE ? urlIconInfo : urlIconError;
+//        URL urlIcon = messageType == INFORMATION_MESSAGE ? urlIconInfo : urlIconError;
         JOptionPane.showMessageDialog(parent, message,
-                "Thông báo", messageType, new ImageIcon(urlIcon));
+                "Thông báo", messageType, null);
     }
 
     public static boolean confirm(Component parent, String message) {
         int result = JOptionPane.showConfirmDialog(parent, message,
                 "Thông báo",
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, new ImageIcon(urlIconInfo));
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null);
         return result == JOptionPane.YES_OPTION;
     }
 
     public static String prompt(Component parent, String message) {
         return (String) JOptionPane.showInputDialog(parent, message,
-                "Thông báo", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(urlIconInfo), null, null);
+                "Thông báo", JOptionPane.INFORMATION_MESSAGE, null, null, null);
     }
 }
